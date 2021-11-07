3b828738a588c0c1ea51733b6938dfc81cecaed0
Nemirtingas <Nemirtingas@noreply.com>
2021-11-07 18:14:07 +0100

    Remove unavailable shared_library_load and shared_library_unload.
    When put in a static library, it is not built because of missing symbols but it doesn't fail either.

--- include/System/SystemExports.h
+++ include/System/SystemExports.h
@@ -82,7 +82,44 @@
     //#define LOCAL_API __attribute__((visibility ("internal")))
 #endif
 
-#ifdef WANT_SYSTEM_SHARED_LIBRARY_EXPORTS
-void shared_library_load();
-void shared_library_unload();
+/*
+Copy/Paste this code in some source file if you want to call a function when the shared_library is loaded/unloaded.
+
+#if defined(SYSTEM_OS_WINDOWS)
+
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        shared_library_load();
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        shared_library_unload();
+        break;
+    }
+    return TRUE;
+}
+
+#elif defined(SYSTEM_OS_LINUX) || defined(SYSTEM_OS_APPLE)
+
+__attribute__((constructor)) SYSTEM_HIDE_API(void, SYSTEM_CALL_DEFAULT) system_shared_library_constructor()
+{
+    shared_library_load();
+}
+
+__attribute__((destructor)) SYSTEM_HIDE_API(void, SYSTEM_CALL_DEFAULT) system_shared_library_destructor()
+{
+    shared_library_unload();
+}
+
 #endif
+*/
