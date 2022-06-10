f8ec153a6f7491543b043c6b3b3357172de36db7
Nemirtingas <Nemirtingas@noreply.com>
2022-06-10 16:29:18 +0200

    Add more archs.

--- include/System/SystemDetector.h
+++ include/System/SystemDetector.h
@@ -19,28 +19,47 @@
 
 #pragma once
 
-#if defined(WIN64) || defined(_WIN64) || defined(__MINGW64__)
+#if defined(WIN64) || defined(_WIN64) || defined(__MINGW64__) || defined(WIN32) || defined(_WIN32) || defined(__MINGW32__)
     #define SYSTEM_OS_WINDOWS
-    #define SYSTEM_ARCH_X64
 
-#elif defined(WIN32) || defined(_WIN32) || defined(__MINGW32__)
-    #define SYSTEM_OS_WINDOWS
-    #define SYSTEM_ARCH_X86
-#elif defined(__linux__) || defined(linux)
-    #if defined(__x86_64__)
-        #define SYSTEM_OS_LINUX
-        #define SYSTEM_ARCH_X64
-    #else
-        #define SYSTEM_OS_LINUX
+    #if defined(_M_IX86)
         #define SYSTEM_ARCH_X86
+    #elif defined(_M_AMD64)
+        #define SYSTEM_ARCH_X64
+    #elif defined(_M_ARM)
+        #define SYSTEM_ARCH_ARM
+    #elif defined(_M_ARM64)
+        #define SYSTEM_ARCH_ARM64
+    #else
+        #error "Unhandled arch"
+    #endif
+#elif defined(__linux__) || defined(linux)
+    #define SYSTEM_OS_LINUX
+
+    #if defined(__i386__) || defined(__i386) || defined(i386)
+        #define SYSTEM_ARCH_X86
+    #elif defined(__x86_64__) || defined(__x86_64) || defined(__amd64) || defined(__amd64__)
+        #define SYSTEM_ARCH_X64
+    #elif defined(__arm__)
+        #define SYSTEM_ARCH_ARM
+    #elif defined(__aarch64__)
+        #define SYSTEM_ARCH_ARM64
+    #else
+        #error "Unhandled arch"
     #endif
 #elif defined(__APPLE__)
-    #if defined(__x86_64__)
-        #define SYSTEM_OS_APPLE
-        #define SYSTEM_ARCH_X64
-    #else
-        #define SYSTEM_OS_APPLE
+    #define SYSTEM_OS_APPLE
+
+    #if defined(__i386__) || defined(__i386) || defined(i386)
         #define SYSTEM_ARCH_X86
+    #elif defined(__x86_64__) || defined(__x86_64) || defined(__amd64) || defined(__amd64__)
+        #define SYSTEM_ARCH_X64
+    #elif defined(__arm__)
+        #define SYSTEM_ARCH_ARM
+    #elif defined(__aarch64__)
+        #define SYSTEM_ARCH_ARM64
+    #else
+        #error "Unhandled arch"
     #endif
 #else
     //#error "Unknown OS"
@@ -58,6 +77,8 @@ namespace System {
     enum class Arch {
         x86 = 0,
         x64 = 1,
+		arm = 2,
+		aarch64 = 3,
     };
 
     static constexpr OperatingSystem os =
@@ -73,7 +94,11 @@ namespace System {
 #if defined(SYSTEM_ARCH_X86)
     Arch::x86;
 #elif defined(SYSTEM_ARCH_X64)
-    Arch::x64;
+	Arch::x64;
+#elif defined(SYSTEM_ARCH_ARM)
+	Arch::arm;
+#elif defined(SYSTEM_ARCH_ARM64)
+    Arch::aarch64;
 #endif
 
 }
