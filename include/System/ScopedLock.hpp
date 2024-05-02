0f75147e0dc6d450d79ae1c252dda9df642602b4
Nemirtingas <nemirtingas@noreply.com>
2024-05-02 11:25:49 +0200

    Rework scoped lock

--- include/System/ScopedLock.hpp
+++ include/System/ScopedLock.hpp
@@ -24,73 +24,46 @@
 
 namespace System {
 
-class scoped_lock {
-    struct value_holder
+template<typename... Args>
+class ScopedLock
+{
+    template<std::size_t I = 0, typename... Tp>
+    inline typename std::enable_if<I == sizeof...(Tp), void>::type unlock(std::tuple<Tp...>& t) { }
+
+    template<std::size_t I = 0, typename... Tp>
+    inline typename std::enable_if < I < sizeof...(Tp), void>::type unlock(std::tuple<Tp...>& t)
     {
-        virtual ~value_holder() noexcept {}
-    };
+        std::get<I>(t).unlock();
+        unlock<I + 1, Tp...>(t);
+    }
 
-    template<typename... Args>
-    struct templated_value_holder : value_holder
-    {
-        template<std::size_t I = 0, typename... Tp>
-        inline typename std::enable_if<I == sizeof...(Tp), void>::type unlock(std::tuple<Tp...>& t) { }
-
-        template<std::size_t I = 0, typename... Tp>
-        inline typename std::enable_if<I < sizeof...(Tp), void>::type unlock(std::tuple<Tp...>& t)
-        {
-            std::get<I>(t).unlock();
-            unlock<I + 1, Tp...>(t);
-        }
-
-        explicit templated_value_holder(Args&... mutexes) : _mutexes(mutexes...) { std::lock(mutexes...); }
-        explicit templated_value_holder(std::adopt_lock_t, Args&... mutexes) : _mutexes(mutexes...) {} // construct but don't lock
-
-        virtual ~templated_value_holder() noexcept { unlock(_mutexes); }
-
-        std::tuple<Args&...> _mutexes;
-    };
-
-    template<typename Arg>
-    struct templated_value_holder<Arg> : value_holder
-    {
-        explicit templated_value_holder(Arg& mutex) : _mutex(mutex) { _mutex.lock(); }
-        explicit templated_value_holder(std::adopt_lock_t, Arg& mutex) : _mutex(mutex) {} // construct but don't lock
-
-        virtual ~templated_value_holder() noexcept { _mutex.unlock(); }
-
-        Arg& _mutex;
-    };
-
-    value_holder* _val;
+    std::tuple<Args&...> _mutexes;
 
 public:
-    template<typename... Args>
-    explicit scoped_lock(Args&... mutexes) : _val(new templated_value_holder<Args&...>(mutexes...)) { }
-
-    template<typename... Args>
-    explicit scoped_lock(std::adopt_lock_t, Args&... mutexes) : _val(new templated_value_holder<Args&...>(std::adopt_lock, mutexes...)) { }
-
-    explicit scoped_lock(scoped_lock && other):
-        _val(other._val)
+    explicit ScopedLock(Args&... mutexes) : _mutexes(mutexes...)
     {
-        other._val = nullptr;
+        std::lock(mutexes...);
     }
 
-    scoped_lock() noexcept:
-        _val(nullptr)
+    explicit ScopedLock(std::adopt_lock_t, Args&... mutexes) : _mutexes(mutexes...)
     {}
-    ~scoped_lock() noexcept { delete _val; }
-    
-    scoped_lock& operator=(scoped_lock && other)
+
+    ~ScopedLock()
     {
-        _val = other._val;
-        other._val = nullptr;
-        return *this;
+        unlock(_mutexes);
     }
 
-    scoped_lock(const scoped_lock&) = delete;
-    scoped_lock& operator=(const scoped_lock&) = delete;
+    ScopedLock(ScopedLock&&) noexcept = default;
+    ScopedLock& operator=(ScopedLock&&) noexcept = default;
+
+    ScopedLock(ScopedLock const&) = delete;
+    ScopedLock& operator=(ScopedLock const&) = delete;
 };
+
+template<typename... Args>
+ScopedLock<Args...> ScopeLock(Args&... mutexes)
+{
+    return ScopedLock<Args...>(mutexes...);
+}
 
 }
